94934d22e19978f238301179ba79ed7c046f8c25
Charles <cethy.hnc@gmail.com>
2024-12-04 09:35:13 +0000

    ADDED: Day 3, part 2 - OK

--- day3/main.go
+++ day3/main.go
@@ -13,20 +13,30 @@ func main() {
 		panic(err)
 	}
 
-	r := regexp.MustCompile(`mul\(([1-9][0-9]?[0-9]?),([1-9][0-9]?[0-9]?)\)`)
+	r := regexp.MustCompile(`mul\(([1-9][0-9]?[0-9]?),([1-9][0-9]?[0-9]?)\)|(do\(\))|(don't\(\))`)
 	matches := r.FindAllStringSubmatch(string(raw), -1)
 
 	res := 0
+	enabled := true
 	for _, match := range matches {
-		l, err := strconv.Atoi(match[1])
-		if err != nil {
-			log.Fatal(err)
+		if match[3] == "do()" {
+			enabled = true
 		}
-		r, err := strconv.Atoi(match[2])
-		if err != nil {
-			log.Fatal(err)
+		if match[4] == "don't()" {
+			enabled = false
 		}
-		res += l * r
+
+		if enabled && match[1] != "" && match[2] != "" {
+			l, err := strconv.Atoi(match[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			r, err := strconv.Atoi(match[2])
+			if err != nil {
+				log.Fatal(err)
+			}
+			res += l * r
+		}
 	}
 
 	log.Println(res)
