2098ef79aa01d6a01a25c79d431925faf61b8f84
Charles <cethy.hnc@gmail.com>
2024-12-09 19:38:39 +0000

    ADDED: Day 9, part 2 - OK

--- day9/main.go
+++ day9/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 )
 
@@ -20,7 +21,7 @@ func diskMapToBlocks(raw string) []int {
 			for j := 0; j < l; j++ {
 				blocks = append(blocks, fileID)
 			}
-			fmt.Println("file : length:", l, " ID: ", fileID)
+			//fmt.Println("file : length:", l, " ID: ", fileID)
 			fileID++
 		case 1: // free space
 			for j := 0; j < l; j++ {
@@ -28,7 +29,7 @@ func diskMapToBlocks(raw string) []int {
 				blocks = append(blocks, -1)
 			}
 
-			fmt.Println("space: length:", l)
+			//fmt.Println("space: length:", l)
 		}
 	}
 
@@ -80,26 +81,11 @@ func displayBlocks(blocks []int) string {
 	return output
 }
 
-func main() {
-	r, err := os.ReadFile("./input.txt")
-	if err != nil {
-		panic(err)
-	}
-	raw := string(r)
-
-	//raw = "2333133121414131402"
-	//raw = "12345"
-
+func part1(raw string) []int {
 	blocks := diskMapToBlocks(raw)
-
-	//fmt.Println(displayBlocks(blocks), "00...111...2...333.44.5555.6666.777.888899" == displayBlocks(blocks))
-	//fmt.Println(displayBlocks(blocks), "0..111....22222" == displayBlocks(blocks))
-
-	//moving file blocks
 
 	// maximum permutations
 	sCount := getSpacesCount(blocks)
-	fmt.Println(sCount)
 
 	for i := 0; i < sCount; i++ {
 		lfi := lastFileBlockIndex(blocks)
@@ -114,7 +100,130 @@ func main() {
 		//fmt.Println(displayBlocks(blocks), "0099811188827773336446555566.............." == displayBlocks(blocks))
 		//fmt.Println(displayBlocks(blocks), "022111222......" == displayBlocks(blocks))
 	}
+	return blocks
+}
 
+func part2displayBlocks(diskmap []struct {
+	size int
+	id   int
+}) string {
+	var output string
+	for _, item := range diskmap {
+		if item.id == -1 {
+			for j := 0; j < item.size; j++ {
+				output += "."
+			}
+		} else {
+			for j := 0; j < item.size; j++ {
+				output += strconv.Itoa(item.id)
+			}
+		}
+	}
+
+	return output
+}
+
+func part2(raw string) []int {
+	var diskmap []struct {
+		//index int
+		size int
+		id   int // if id=-1 => space
+	}
+
+	var fileID int
+	for i, ru := range raw {
+		l, err := strconv.Atoi(string(ru))
+		if err != nil {
+			panic(err)
+		}
+
+		currID := -1 // identify space
+		if i%2 == 0 {
+			currID = fileID
+			fileID++
+		}
+		diskmap = append(diskmap, struct{ size, id int }{size: l, id: currID})
+	}
+	//fmt.Println(diskmap)
+
+	var maxFileID int
+	if diskmap[len(diskmap)-1].id > maxFileID {
+		maxFileID = diskmap[len(diskmap)-1].id
+	}
+	if diskmap[len(diskmap)-2].id > maxFileID {
+		maxFileID = diskmap[len(diskmap)-2].id
+	}
+
+	//fmt.Println(part2displayBlocks(diskmap))
+	//fmt.Println()
+
+	// for each file, last to first
+	for curFileID := maxFileID; curFileID >= 0; curFileID-- {
+		var file struct{ size, id int }
+		fileIndex := -1
+		for i, item := range diskmap {
+			if item.id == curFileID {
+				file = item
+				fileIndex = i
+			}
+		}
+
+		// for each space, first to "file index"
+		for spaceIndex := 0; spaceIndex < fileIndex; spaceIndex++ {
+			if diskmap[spaceIndex].id != -1 {
+				continue
+			}
+
+			space := diskmap[spaceIndex]
+
+			// if space can fit file
+			if space.size >= file.size {
+				// replace file with a space of same size
+				diskmap[fileIndex].id = -1
+
+				// put file before space
+				diskmap[spaceIndex].id = file.id
+				diskmap[spaceIndex].size = file.size
+				//fmt.Println(part2displayBlocks(diskmap), " move file", file.id)
+
+				// insert new space if needed
+				if space.size != file.size {
+					diskmap = slices.Insert(diskmap, spaceIndex+1, struct {
+						size int
+						id   int
+					}{size: space.size - file.size, id: -1})
+				}
+				//fmt.Println(part2displayBlocks(diskmap), " fix space")
+
+				break
+			}
+		}
+	}
+
+	//fmt.Println("\n00992111777.44.333....5555.6666.....8888..")
+	//fmt.Println(part2displayBlocks(diskmap), "00992111777.44.333....5555.6666.....8888.." == part2displayBlocks(diskmap))
+
+	var blocks []int
+	for _, item := range diskmap {
+		for i := 0; i < item.size; i++ {
+			blocks = append(blocks, item.id)
+		}
+	}
+
+	return blocks
+}
+
+func main() {
+	r, err := os.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	raw := string(r)
+
+	raw = "2333133121414131402"
+	//raw = "12345"
+
+	blocks := part1(raw)
 	// checksum
 	var checksum int
 	for i, fileID := range blocks {
@@ -123,5 +232,17 @@ func main() {
 		}
 		checksum += fileID * i
 	}
-	fmt.Println("checksum: ", checksum)
+	fmt.Println("checksum (part1): ", checksum)
+
+	blocks = part2(raw)
+
+	// checksum
+	checksum = 0
+	for i, fileID := range blocks {
+		if fileID == -1 {
+			continue
+		}
+		checksum += fileID * i
+	}
+	fmt.Println("checksum (part2): ", checksum)
 }
